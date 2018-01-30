52e00f98021dc9e17f96e0eecee7c0d6eb1456e4
Bunjigai21 <romain.nicoli83@gmail.com>
2018-01-30 18:52:00 +0100

    first

---
+++ SA3_pure-bio-143/modules/ganalytics/translations/fr.php
@@ -0,0 +1,5 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ganalytics}sa3_pure-bio-143>ganalytics_f4f70727dc34561dfde1a3c529b6205c'] = 'Paramètres';
---
+++ SA3_pure-bio-143/modules/graphnvd3/translations/fr.php
@@ -0,0 +1,5 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{graphnvd3}sa3_pure-bio-143>graphnvd3_a9f70dff230e6fc8e878043486e6cddf'] = 'Graphiques NVD3';
